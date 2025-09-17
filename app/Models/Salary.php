1997f8064fd88841464b7f1d211d6f776ff8e776
clark <clarkkentibale@gmail.com>
2025-09-17 15:23:52 +0800

    time: 41:35 - Done by making a lot of Livewire Admin's

--- app/Models/Salary.php
+++ app/Models/Salary.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Salary extends Model
 {
-    //
+    protected $fillable = ['payroll_id', 'employee_id', 'gross_salary'];
+
+    public function payroll(): BelongsTo
+    {
+        return $this->belongsTo(Payroll::class);
+    }
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Admin;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -7,10 +8,57 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function(): void{
+    Route::get('/dashboard', Admin\Dashboard::class)->name('dashboard');
 
+        // Companies
+        Route::prefix('companies')->name('companies.')->group(function (): void {
+            Route::get('/', Admin\Companies\Index::class)->name('index');
+            Route::get('/create', Admin\Companies\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Companies\Edit::class)->name('edit');
+        });
+
+        Route::middleware('company.context')->group(function(){
+            
+        // Departments
+        Route::prefix('departments')->name('departments.')->group(function (): void {
+            Route::get('/', Admin\Departments\Index::class)->name('index');
+            Route::get('/create', Admin\Departments\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Departments\Edit::class)->name('edit');
+        });
+
+        // Designations
+        Route::prefix('designations')->name('designations.')->group(function (): void {
+            Route::get('/', Admin\Designations\Index::class)->name('index');
+            Route::get('/create', Admin\Designations\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Designations\Edit::class)->name('edit');
+        });
+
+        // Employees
+        Route::prefix('employees')->name('employees.')->group(function (): void {
+            Route::get('/', Admin\Employees\Index::class)->name('index');
+            Route::get('/create', Admin\Employees\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Employees\Edit::class)->name('edit');
+        });
+
+        // Contracts
+        Route::prefix('contracts')->name('contracts.')->group(function (): void {
+            Route::get('/', Admin\Contracts\Index::class)->name('index');
+            Route::get('/create', Admin\Contracts\Create::class)->name('create');
+            Route::get('/{id}/edit', Admin\Contracts\Edit::class)->name('edit');
+        });
+
+        // Payrolls
+        Route::prefix('payrolls')->name('payrolls.')->group(function (): void {
+            Route::get('/', Admin\Payrolls\Index::class)->name('index');
+            Route::get('/create', Admin\Payrolls\Show::class)->name('create');
+        });
+    });
+
+});/* ->name( 'dashboard'); */
+
+
+    
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
