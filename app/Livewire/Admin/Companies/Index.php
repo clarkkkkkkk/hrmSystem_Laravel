1159f42c3262e401296434f0213cdf4611f10206
clark <clarkkentibale@gmail.com>
2025-09-19 22:20:28 +0800

    ,

--- app/Livewire/Admin/Companies/Index.php
+++ app/Livewire/Admin/Companies/Index.php
@@ -5,6 +5,8 @@ namespace App\Livewire\Admin\Companies;
 use App\Models\Company;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
+use Livewire\Features\SupportPagination\WithoutUrlPagination;
+use Livewire\WithPagination;
 
 use function Pest\Laravel\delete;
 
@@ -12,6 +14,7 @@ class Index extends Component
 {
     public function delete($id): void
     {
+        use WithPagination, WithoutUrlPagination;
         $company = Company::find($id);
         $company = delete();
         session()->flash('message', 'Company deleted successfully.');
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -35,6 +36,13 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        DB::table('users')->import([
+            'name'=>'Admin',
+            'email'=>'admin@emample.com',
+            'password' => Hash::make('1234567890'),
+        ]);
+
     }
 
     /**
